f934171865041aff821f01a149d353bffe194216
TekinGomes37 <e.gomesmatos@tekin.fr>
2020-11-10 13:52:44 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Gesture, GestureController, GestureDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  private gesture: Gesture;
+  @ViewChild('touch') div: ElementRef<HTMLDivElement>;
+  @ViewChild('paragraph') p: ElementRef<HTMLParagraphElement>;
 
+  constructor(private gestureCtrl: GestureController) {}
+
+  ngAfterViewInit() {
+    this.gesture = this.gestureCtrl.create({
+      disableScroll: true,
+      el: this.div.nativeElement,
+      threshold: 15,
+      gestureName: 'my-gesture',
+      direction: 'y',
+      onMove: ev => this.onMoveHandler(ev)
+    });
+
+    this.gesture.enable();
+  }
+
+  onMoveHandler(ev: GestureDetail) {
+    console.log(ev);
+    this.p.nativeElement.innerHTML = `
+    <div>Type: ${ev.type}</div>
+    <div>Current Y: ${ev.currentY}</div>
+    <div>Delta Y: ${ev.currentY}</div>
+    <div>Velocity Y: ${ev.velocityY}</div>`;
+  }
 }
